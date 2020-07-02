33fb9930195f0520b625a447d157ffd8e852906b
Hadrien Leclerc <hadrien.leclerc@insee.fr>
2020-07-02 11:29:28 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo " \
 \nAWS_S3_ENDPOINT=$AWS_S3_ENDPOINT \
 \nAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 \nAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-\nAWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-" >> /usr/local/lib/R/etc/Renviron.site \
+\nAWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+" >> /usr/local/lib/R/etc/Renviron.site 
 exec @$
