a763683bd6f8b1f99f7bac7d98cd6706072531db
hadrien_leclerc <hadrien.leclerc@insee.fr>
2020-07-03 13:19:34 +0200

    DB_variable update from minio

--- function/update_DB_variable.R
+++ function/update_DB_variable.R
@@ -28,54 +28,60 @@ update_DB_variable <- function(){
       pull(minio_path) %>% 
       as.character()
     
-    if(stringr::str_detect(obj_name, "_gg_plot$")){
-      read_function = readRDS
-    }
-    if(stringr::str_detect(obj_name, "_png$")){
-      read_function = png::readPNG
-    }
-    if(stringr::str_detect(obj_name, "_jpeg$")){
-      read_function = jpeg::readJPEG
-    }
-    
-    minio_obj = try(aws.s3::s3read_using(FUN = read_function,
-                             bucket = "groupe-1360",
-                             object = obj_name,
-                             opts = list("use_https" = F, "region" = "")))
-    var_title <- ""
-    gg_run_time <- ""
-    gg_code_file <- ""
-    
-    if(!"try-error" %in% class(minio_obj)){
+    if(stringr::str_detect(obj_name, "_gg_plot$|_png_title$|_jpeg_title$")){
+      minio_obj = try(aws.s3::s3read_using(FUN = readRDS,
+                                           bucket = "groupe-1360",
+                                           object = obj_name,
+                                           opts = list("use_https" = F, "region" = "")))
+      var_title <- ""
+      gg_run_time <- ""
+      gg_code_file <- ""
       
-      gg = minio_obj
-      gg_run_time <- gg$run_time
-      
-      if(is.null(gg_run_time)){gg_run_time <- ""}
-      
-      gg_code_file <- gg$link_code_file
-      if(is.null(gg_code_file)){gg_code_file <- ""}
-      
-      if("ggplot" %in% class(gg)){
-        var_title = gg$labels$title
+      if(!"try-error" %in% class(minio_obj)){
+        
+        if("ggplot" %in% class(minio_obj) | "highchart" %in% class(minio_obj)){
+          gg = minio_obj
+          gg_run_time <- gg$run_time
+          
+          if(is.null(gg_run_time)){gg_run_time <- ""}
+          
+          gg_code_file <- gg$link_code_file
+          if(is.null(gg_code_file)){gg_code_file <- ""}
+          
+          if("ggplot" %in% class(gg)){
+            var_title = gg$labels$title
+          }
+          
+          if("highchart" %in% class(gg)){
+            var_title = gg[["x"]][["hc_opts"]][["title"]][["text"]]
+          }
+          
+          if(is.null(var_title)){
+            var_title = ""
+          }
+          DB_variable[row_id, "title"] = var_title
+          DB_variable[row_id, "run_time"] = as.numeric(as.character(gg_run_time))
+          DB_variable[row_id, "file"] = as.character(gg_code_file)
+        }
+        
+        if(stringr::str_detect(obj_name, "_png_title$|_jpeg_title$")){
+          if("data.frame" %in% class(minio_obj)){
+            row_id_targeted = which(DB_variable[, "minio_path"] == gsub("_title", "", obj_name))
+            if(length(row_id_targeted) > 0){
+              DB_variable[row_id_targeted, "title"] = as.character(minio_obj[1,"title"])
+              DB_variable[row_id_targeted, "run_time"] = 0
+              DB_variable[row_id_targeted, "file"] = ""
+            }
+          }
+        }
       }
-      
-      if("highchart" %in% class(gg)){
-        var_title = gg[["x"]][["hc_opts"]][["title"]][["text"]]
-      }
-      
-      if(is.null(var_title)){
-        var_title = ""
-      }
-      
-      DB_variable[row_id, "title"] = var_title
-      DB_variable[row_id, "run_time"] = as.numeric(as.character(gg_run_time))
-      DB_variable[row_id, "file"] = as.character(gg_code_file)
-      
     }
   }
   
-  s3write_using(DB_variable, FUN = saveRDS,
+  DB_variable = DB_variable %>% 
+    tidyr::drop_na()
+  
+  aws.s3::s3write_using(DB_variable, FUN = saveRDS,
                 bucket = "groupe-1360", object = "dataviz/DB_variable/DB_variable",
                 opts = list("use_https" = F, "region" = ""))
   
