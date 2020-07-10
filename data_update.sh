0c86f04b5c0a2cbf8bd1407ebb3bc9d61b49d31d
Hadrien Leclerc <hadrien.leclerc@insee.fr>
2020-07-10 09:09:12 +0000

    Update data_update.sh

--- data_update.sh
+++ data_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-R_location = $(which R)
+location = $(which R)
 echo "folder : $R_location"
 
-R CMD BATCH /srv/shiny-server/function/data_update.R
+#R CMD BATCH /srv/shiny-server/function/data_update.R
