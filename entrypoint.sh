678d18af24a766e978692f3e22cc4f02cfebceba
Hadrien Leclerc <hadrien.leclerc@insee.fr>
2020-07-16 19:45:41 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ echo -e " \
 \nAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 \nAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
 \nAWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+\nPATH=$PATH
 " >> /usr/local/lib/R/etc/Renviron.site 
 exec "$@"
